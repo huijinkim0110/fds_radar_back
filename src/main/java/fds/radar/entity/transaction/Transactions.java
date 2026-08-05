acc640309181f9f4769ddf5d001b01fe3e02c61e
riiiiiiiw <gunshoot628@gmail.com>
2026-08-05 15:05:22 +0900

    entity_transaction

--- src/main/java/fds/radar/entity/transaction/Transactions.java
+++ src/main/java/fds/radar/entity/transaction/Transactions.java
@@ -1,6 +1,11 @@
 package fds.radar.entity.transaction;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,5 +26,66 @@ public class Transactions {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long accountId;
+
+    @Column(nullable = false)
+    private Long cardId;
+
+    @Column(nullable = false)
+    private Long recipientId;
+
+    @Column(nullable = false)
+    private Long merchantId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_type", nullable = false)
+    private TransactionType transactionType;
+
+    public enum TransactionType {
+        CARD_PAYMNENT,
+        ACCOUNT_TRANSFER
+    }
+
+    @Column(name = "amount", nullable = false)
+    private Long amount; // 원 단위 거래 금액
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_channel", nullable = false)
+    private TransactionChannel transactionChannel;
+
+    public enum TransactionChannel {
+        APP,
+        WEB,
+        ATM
+    }
     
+    @Column(nullable = false)
+    private String countryCode;
+
+    @Column(nullable = false)
+    private String region;
+
+    @Column(nullable = false)
+    private Long deviceId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_status", nullable = false)
+    private String transactionStatus;
+
+    public enum TransactionStatus {
+        APPROVED,
+        PENDING,
+        CANCELED
+    }
+
+    @Column(nullable = false)
+    private LocalDateTime occurredAt; // 거래 발생 시점
+
+    @Column(nullable = false)
+    private LocalDateTime approvedAt; // 거래 승인 시점
 }
