78221e898910e0dccff4facdc55e4e3c9ef5640c
riiiiiiiw <gunshoot628@gmail.com>
2026-08-07 13:04:12 +0900

    TransactionRepository

---
+++ src/main/java/fds/radar/repository/TransactionRepository.java
@@ -0,0 +1,33 @@
+package fds.radar.repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fds.radar.common.TransactionType;
+import fds.radar.entity.transaction.Transactions;
+
+public interface TransactionRepository extends JpaRepository<Transactions, Long>{
+    
+    // 본인 거래 내역만 조회 (거래 상세)
+    Optional<Transactions> findByIdAndUserId(Long id, Long userId);
+
+    // 내 거래내역 - 페이징
+    Page<Transactions> findByUserId(Long userId, Pageable pageable);
+
+    // 유형, 기간 필터 
+    Page<Transactions> findByUserIdAndTypeAndCreatedAtBetween (
+        Long userId, TransactionType type,
+        LocalDateTime from, LocalDateTime to, Pageable pageable);
+
+    // 중복 결제 방지 
+    Optional<Transactions> findByIddemotencyKey (String idempotencyKey);
+
+    // isNewRecipient 판정용 - 이체이력 확인
+    boolean existsByUserIdAndTypeAndRecipientAccountNumber(
+        Long userId, TransactionType type, String recipientAccountNumber
+    );
+}
