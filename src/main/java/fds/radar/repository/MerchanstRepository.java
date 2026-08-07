6752c812fd7f882eb5911c0408f3f4a2e1f05ad5
riiiiiiiw <gunshoot628@gmail.com>
2026-08-07 12:39:50 +0900

    MerchantsRepository

---
+++ src/main/java/fds/radar/repository/MerchanstRepository.java
@@ -0,0 +1,21 @@
+package fds.radar.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fds.radar.entity.transaction.Merchants;
+import fds.radar.common.RiskStatus;
+
+
+public interface MerchanstRepository extends JpaRepository<Merchants, Long>{
+
+    // 등록 시 중복 확인
+    boolean existsByName(String name);
+    Optional<Merchants> findByName(String name);
+
+    // 위험 상태 별 조회 (의심, 차단 가맹점 관리, 모니터링용)
+    List<Merchants> findByRiskStatus(RiskStatus riskStatus);
+    
+}
