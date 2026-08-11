f2a2e246aa2cce1a5481fb321e7e28214f36622f
riiiiiiiw <gunshoot628@gmail.com>
2026-08-11 10:57:10 +0900

    TransferRepository Modify

--- src/main/java/fds/radar/repository/TransferRepository.java
+++ src/main/java/fds/radar/repository/TransferRepository.java
@@ -10,13 +10,13 @@ import fds.radar.entity.account.TransferRecipients;
 public interface TransferRepository extends JpaRepository<TransferRecipients, Long>{
 
    // 내 수취인 목록
-   List<TransferRecipients> findByIdAndUserId(Long userId);
+   List<TransferRecipients> findByUserId(Long userId);
 
    // 본인 소유 검증 - 타인 수취인 삭제 차단
    Optional<TransferRecipients> findByIdAndUserId(Long id, Long userId);
     
-   // 저장 시 중복 방시 - 가틍ㄴ 사람이 같은 은행 + 계좌 중복 저장 안되게
-   boolean existsByUserIdAndBankCodeAndAccountNumber (
-        Long userId, String bankCode, String accountNumber
+   // 저장 시 중복 방시 - 같은 사람이 같은 은행 + 계좌 중복 저장 안되게
+   boolean existsByUserIdAndInstitution_InstitutionIdAndAccountNumber (
+        Long userId, Long institutionId, String accountNumber
    );
 }
