a41260f7f24219c7acc3ea06943e007dd9598c40
huijinkim0110 <huijin1053@gmail.com>
2026-08-11 18:03:18 +0900

    notification오타수정

---
+++ src/main/java/fds/radar/repository/user/NotficationRepository.java
@@ -0,0 +1,13 @@
+package fds.radar.repository.user;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fds.radar.entity.user.Notification;
+
+public interface NotficationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUser_UserIdOrderByCreatedAtDesc(Long userId);
+    List<Notification> findByUser_UserIdAndIsReadFalse(Long userId);
+    
+}
