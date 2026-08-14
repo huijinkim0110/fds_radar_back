c19780b8ca45b608c6c72ab0d2d47ab3c0430766
huijinkim0110 <huijin1053@gmail.com>
2026-08-14 12:14:07 +0900

    금감원 API임시 호출

--- src/main/java/fds/radar/controller/financialProduct/FssProductSyncController.java
+++ src/main/java/fds/radar/controller/financialProduct/FssProductSyncController.java
@@ -1,30 +1,30 @@
-package fds.radar.controller.financialProduct;
+// package fds.radar.controller.financialProduct;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import fds.radar.service.financialProduct.FssProductSyncService;
-import lombok.RequiredArgsConstructor;
+// import fds.radar.service.financialProduct.FssProductSyncService;
+// import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/admin/products")
-@RequiredArgsConstructor
-// 실행용 임시 Controller
-public class FssProductSyncController {
+// @RestController
+// @RequestMapping("/admin/products")
+// @RequiredArgsConstructor
+// // 실행용 임시 Controller
+// public class FssProductSyncController {
     
-    private final FssProductSyncService fssProductSyncService;
+//     private final FssProductSyncService fssProductSyncService;
 
-    @PostMapping("/sync-deposits")
-    public ResponseEntity<String> syncDeposits() {
-        int count = fssProductSyncService.syncDeposits();
-        return ResponseEntity.ok(count + "개 정기예금 상품 저장 완료");
-    }
+//     @PostMapping("/sync-deposits")
+//     public ResponseEntity<String> syncDeposits() {
+//         int count = fssProductSyncService.syncDeposits();
+//         return ResponseEntity.ok(count + "개 정기예금 상품 저장 완료");
+//     }
 
-    @PostMapping("/sync-savings")
-    public ResponseEntity<String> syncSavings() {
-        int count = fssProductSyncService.syncSavings();
-        return ResponseEntity.ok(count + "개 적금 상품 저장 완료");
-    }
-}
+//     @PostMapping("/sync-savings")
+//     public ResponseEntity<String> syncSavings() {
+//         int count = fssProductSyncService.syncSavings();
+//         return ResponseEntity.ok(count + "개 적금 상품 저장 완료");
+//     }
+// }
