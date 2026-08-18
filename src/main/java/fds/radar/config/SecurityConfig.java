74d4137d50c173ab22c1feb46d94259f6cc07d5b
riiiiiiiw <gunshoot628@gmail.com>
2026-08-18 09:40:20 +0900

    Security permitAll

--- src/main/java/fds/radar/config/SecurityConfig.java
+++ src/main/java/fds/radar/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
