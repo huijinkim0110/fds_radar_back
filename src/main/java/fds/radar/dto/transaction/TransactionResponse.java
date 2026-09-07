824079697563b7cb890f88c6e826f2a85307a678
riiiiiiiw <gunshoot628@gmail.com>
2026-09-07 09:58:17 +0900

    transaction 수정

--- src/main/java/fds/radar/dto/transaction/TransactionResponse.java
+++ src/main/java/fds/radar/dto/transaction/TransactionResponse.java
@@ -17,12 +17,15 @@ public class TransactionResponse {
     private TransactionStatus status;
     private LocalDateTime occurredAt;
     private LocalDateTime approvedAt;
+    private String merchantName;   // 카드결제일 때만 값 있음
+    private String recipientName;  // 계좌이체일 때만 값 있음
 
     public TransactionResponse() {}
 
     public TransactionResponse(Long transactionId, TransactionType transactionType, BigDecimal amount,
                                TransactionChannel channel, TransactionStatus status,
-                               LocalDateTime occurredAt, LocalDateTime approvedAt) {
+                               LocalDateTime occurredAt, LocalDateTime approvedAt,
+                               String merchantName, String recipientName) {
         this.transactionId = transactionId;
         this.transactionType = transactionType;
         this.amount = amount;
@@ -30,6 +33,8 @@ public class TransactionResponse {
         this.status = status;
         this.occurredAt = occurredAt;
         this.approvedAt = approvedAt;
+        this.merchantName = merchantName;
+        this.recipientName = recipientName;
     }
 
     public static TransactionResponse from(Transactions tx) {
@@ -40,15 +45,19 @@ public class TransactionResponse {
             tx.getTransactionChannel(),
             tx.getTransactionStatus(),
             tx.getOccurredAt(),
-            tx.getApprovedAt()
+            tx.getApprovedAt(),
+            tx.getMerchant() != null ? tx.getMerchant().getMerchantName() : null,
+            tx.getRecipient() != null ? tx.getRecipient().getRecipientName() : null
         );
     }
 
-    public Long gettransactionId() { return transactionId; }
+    public Long getTransactionId() { return transactionId; } // [D파트 담당자 수정] 대소문자 수정
     public TransactionType getTransactionType() { return transactionType; }
     public BigDecimal getAmount() { return amount; }
     public TransactionChannel getChannel() { return channel; }
     public TransactionStatus getStatus() { return status; }
     public LocalDateTime getOccurredAt() { return occurredAt; }
     public LocalDateTime getApprovedAt() { return approvedAt; }
+    public String getMerchantName() { return merchantName; }
+    public String getRecipientName() { return recipientName; }
 }
