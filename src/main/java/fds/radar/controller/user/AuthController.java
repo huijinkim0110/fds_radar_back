b805b8e3980d9d0d19633a2af0aedbe4f93838f0
vinxx0 <vxxixo8743@gmail.com>
2026-09-09 17:17:23 +0900

    비밀번호 찾기 기능 추가

--- src/main/java/fds/radar/controller/user/AuthController.java
+++ src/main/java/fds/radar/controller/user/AuthController.java
@@ -1,5 +1,7 @@
 package fds.radar.controller.user;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fds.radar.dto.user.EmailCheckRequest;
 import fds.radar.dto.user.LoginRequest;
 import fds.radar.dto.user.LoginResponse;
 import fds.radar.dto.user.SignUpRequest;
 import fds.radar.dto.user.SignUpResponse;
+import fds.radar.dto.user.PasswordResetRequest;
 import fds.radar.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -42,5 +46,27 @@ public class AuthController {
         LoginResponse response = userService.login(request);
 
         return ResponseEntity.ok(response);
+    }
+
+    //  비밀번로 재설정
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> resetPassword(
+        @Valid @RequestBody PasswordResetRequest request) {
+
+            userService.resetPassword(request);
+
+            return ResponseEntity.ok("비밀번호가 변경되었습니다.");
+        }
+
+    // 가입된 이메일 확인
+    @PostMapping("/check-email")
+    public ResponseEntity<Map<String, Boolean>> checkEmail(
+            @Valid @RequestBody EmailCheckRequest request) {
+
+        boolean exists = userService.checkEmail(request.getEmail());
+
+        return ResponseEntity.ok(
+                Map.of("exists", exists)
+        );
     }
 }
--- src/main/java/fds/radar/repository/user/UserRepository.java
+++ src/main/java/fds/radar/repository/user/UserRepository.java
@@ -8,6 +8,8 @@ import fds.radar.entity.user.Users;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
+    Optional<Users> findByPhone(String phone); // 전화번호로 회원 조회
+
     Optional<Users> findByEmail(String Email); // 로그인시 이메일로 유저 조회
     boolean existsByEmail(String email); // 회원가입 시 중복 이메일 체크
  
