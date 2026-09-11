1c55f4b3a9dc03546f92d7417a41962cba67a9b1
Hyewonzz <flqhs0312@naver.com>
2026-09-11 12:55:56 +0900

    feat: 신고 사유(선택형)와 상세내용을 분리하는 reasonCategory 필드 추가

--- src/main/java/fds/radar/dto/fraud/FraudReportRequest.java
+++ src/main/java/fds/radar/dto/fraud/FraudReportRequest.java
@@ -5,5 +5,6 @@ import lombok.Getter;
 @Getter
 public class FraudReportRequest {
     private Long transactionId;
-    private String reason;
+    private String reasonCategory; // [D파트 담당자 추가] 선택형 사유
+    private String reason; // 상세 내용 (기존 필드, 의미를 "상세 내용"으로 명확히)
 }
